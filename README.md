f8da42847cb56e7fe1b7ab102e576d913876aa5c
CHEF-KOCH <Nvinsidemail.com>
2015-11-01 18:03:10 +0100

    Initial bump, hello and welcome
    Signed-off-by: CHEF-KOCH <Nvinsidemail.com>

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Tor-Messenger-user.js
-Hardens the Tor Messenger user.js for maximum possible security 
+
+This will harden the Tor Messenger user.js for maximum possible security.
+
+
+This is original released by CHEF-KOCH 2015 under the GNU GENERAL PUBLIC LICENSE v2 license. 
+
+
+Please note that the entire messenger is currently in beta, so everything can be changed with newer versions!
+
+
+How can I access the about:config stuff under Tor Messenger?
+-------------------
+
+* Options - Advanced - General - 'Config Editor'
+* The prefs.js is located under: ~\Tor Messenger\Messenger\TorMessenger\Data\Browser\profile.default\
+
+
+Some security things are enabled, why?
+-------------------
+
+* Because I don't disable everything due the fact that to disable it have no real benefit except theoretically attacks. If you found real pages with attacks, report it to me and I will posible change that, but as long no one found any page / link which compromise the securty aspect within Tor Messenger I will not change that. I really don't a damn about theoretically pages which shows xyz possible attack which mostly requires Javascript and such enabled. 
+* E.g. safebrowsing/cache/ssl reporting/javascript is enabled for several reasons.
+
+
+
+Some options are removed will you add it?
+-------------------
+
+* No, my philosophy is to always use the latest software and use it with latest security options, so I won't look behind and add xyz config because someone want to use an outdated version. No, sorry at this point, your own your own if do so.
---
+++ prefs.js
@@ -0,0 +1,45 @@
+# Mozilla User Preferences
+
+/* This is an hardened version by CHEF-KOCh and his Tor-Messenger-user.js project
+ * https://github.com/CHEF-KOCH/Tor-Messenger-user.js
+ */
+
+user_pref("app.update.auto", true);
+user_pref("app.update.enabled", true);
+user_pref("beacon.enabled", false);
+user_pref("breakpad.reportURL", "");
+user_pref("browser.search.geoip.url", "");
+user_pref("browser.search.update", true);
+user_pref("canvas.filters.enabled", true);
+user_pref("dom.battery.enabled", false);
+user_pref("dom.enable_performance", false);
+user_pref("dom.event.clipboardevents.enabled", false);
+user_pref("extensions.blocklist.pingCountVersion", 0);
+user_pref("extensions.lastPlatformVersion", "41.0");
+user_pref("extensions.pendingOperations", false);
+user_pref("extensions.shownSelectionUI", true);
+user_pref("extensions.torlauncher.prompt_at_startup", false);
+user_pref("extensions.ui.dictionary.hidden", true);
+user_pref("extensions.ui.locale.hidden", true);
+user_pref("extensions.update.autoUpdateDefault", true);
+user_pref("extensions.update.enabled", true);
+user_pref("general.warnOnAboutConfig", false);
+user_pref("network.cookie.cookieBehavior", 2);
+user_pref("network.cookie.lifetime.days", 2);
+user_pref("network.cookie.prefsMigrated", true);
+user_pref("network.dns.disableIPv6", true);
+user_pref("network.dnsCacheEntries", 200);
+user_pref("network.http.spdy.enabled.deps", false);
+user_pref("network.http.spdy.enabled.http2", false);
+user_pref("network.http.spdy.enabled.v3-1", false);
+user_pref("network.http.speculative-parallel-limit", 2);
+user_pref("network.predictor.cleaned-up", true);
+user_pref("noscript.forbidFlash", true);
+user_pref("noscript.forbidFonts", true);
+user_pref("noscript.forbidJava", true);
+user_pref("noscript.forbidSilverlight", true);
+user_pref("security.ssl3.ecdhe_ecdsa_rc4_128_sha", false);
+user_pref("security.ssl3.ecdhe_rsa_rc4_128_sha", false);
+user_pref("security.ssl3.rsa_rc4_128_md5", false);
+user_pref("social.whitelist", "");
+
